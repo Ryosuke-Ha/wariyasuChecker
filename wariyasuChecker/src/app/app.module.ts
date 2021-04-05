9d962409a24e31a61d2ab59efd18c6ac131c0057
Ryosuke-Ha <ryosuke.0815@icloud.com>
2021-04-05 20:46:08 +0900

    feat(component):add libraries

--- wariyasuChecker/src/app/app.module.ts
+++ wariyasuChecker/src/app/app.module.ts
@@ -8,24 +8,40 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 // component
 import { TopPageComponent } from './component/top-page/top-page.component';
 import { StockListsComponent } from './component/stock-lists/stock-lists.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { LoginComponent } from './component/login/login.component';
+import { HomeComponent } from './component/home/home.component';
 
 //angular material
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTableModule } from '@angular/material/table';
-
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopPageComponent,
-    StockListsComponent
+    StockListsComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatTableModule
+    MatTableModule,
+    MatTabsModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
